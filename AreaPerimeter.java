6bd96f87612c741574f41cad86b589502a0dbf08
Ajeet Singh <imajeet5@gmail.com>
2021-06-30 11:57:58 +0530

    First Commit

---
+++ AreaPerimeter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class AreaPerimeter {
+
+    public static void main(String[] args) {
+         double r;
+         double p,a;
+         Scanner scn = new Scanner(System.in);
+         System.out.println("Enter the radius of circle");
+         r= scn.nextDouble();
+         p= 2* Math.PI*r;
+         System.out.println("the perimeter of circle is "+p);  
+         a=  Math.PI*r*r;
+         System.out.println("the perimeter of circle is "+a); 
+         // Added new comments
+    }
+
+}
